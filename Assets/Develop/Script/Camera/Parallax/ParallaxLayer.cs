28a5fa73738ebb5a6a578992baa46c7c50572e8b
SeoBeomSeok <warcry4523@gmail.com>
2023-10-19 15:18:28 +0900

    refactor : 패럴랙스 레이어 코드 개선
    태그와 레이어, 솔팅 레이어를 레이어 타입에 따라 변경되도록 개선함.
    일일이 변경하면 번거롭기 때문에 편의성 위주로 개선하였음.
    
    또한 헤더 속성으로 변수에 대한 설명이 직관적으로 들어오게 바꾸었음.
    스크롤 속도를 슬라이더 형태로 바꾸었고, 사이즈도 슬라이더를 통해 변경 할 수 있게 개선하였음.

--- Assets/Develop/Script/Camera/Parallax/ParallaxLayer.cs
+++ Assets/Develop/Script/Camera/Parallax/ParallaxLayer.cs
@@ -2,25 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public enum LayerType
 {
+    [InspectorName("기본값")]
+    Default,
+    [InspectorName("원경")]
     Background,
+    [InspectorName("중경")]
     Middleground,
+    [InspectorName("근경")]
     Foreground
 }
 
-[AddComponentMenu("패럴랙스 스크롤/패럴랙스 레이어")]
+[AddComponentMenu("패럴랙스 스크롤/패럴랙스 레이어"), ExecuteInEditMode]
 public class ParallaxLayer : MonoBehaviour
 {
+    [Header("오브젝트 타입 설정")]
     [SerializeField, Tooltip("오브젝트의 타입을 정합니다.")]
     private LayerType _layerType;
-    [SerializeField, Tooltip("오브젝트의 수평 이동 속도를 조절합니다.")] private float _horizontalFactor;
-    [SerializeField, Tooltip("오브젝트의 수직 이동 속도를 조절합니다.")] private float _verticalFactor;
+    [Header("오브젝트 x축 속도 설정")]
+    [SerializeField, Tooltip("오브젝트의 수평 이동 속도를 조절합니다."), Range(-2f, 2f)] private float _horizontalFactor;
+    [Header("오브젝트 y축 속도 설정")]
+    [SerializeField, Tooltip("오브젝트의 수직 이동 속도를 조절합니다."),Range(-1f, 1f)] private float _verticalFactor;
+    [Header("오브젝트 크기 설정")]
+    [SerializeField, Tooltip("스프라이트의 사이즈를 조절합니다."), Range(-2f, 3f)]
+    private float _spriteSize;
     private SpriteRenderer _renderer;
-    private Rigidbody2D rigid;
     
+    [Space(20f)]
     [SerializeField]
     private Transform _leftPoint;
     [SerializeField]
@@ -29,52 +41,80 @@ public class ParallaxLayer : MonoBehaviour
     private Transform _upPoint;
     [SerializeField]
     private Transform _downPoint;
+
+    private Vector2 _leftWall;
+    private Vector2 _rightWall;
+    private Vector2 _upWall;
+    private Vector2 _downWall;
     
 
     private void Awake()
     {
         _renderer = GetComponent<SpriteRenderer>();
-        rigid = GetComponent<Rigidbody2D>();
-        if (_layerType == LayerType.Background)
-        {
-            float posX = transform.position.x;
-            float posY = transform.position.y;
-            _leftPoint.position = new Vector2((posX - (_renderer.size.x / 2) * transform.localScale.x),posY);
-            _rightPoint.position = new Vector2((posX + (_renderer.size.x / 2) * transform.localScale.x),posY);
-            _upPoint.position = new Vector2(posX,posY+ (_renderer.size.y / 2) * transform.localScale.y);
-            _downPoint.position = new Vector2(posX,posY - (_renderer.size.y / 2)*transform.localScale.y);
-        }
+        
     }
 
-    void SetLayerSize()
+    private void Start()
+    {
+        float posX = transform.position.x;
+        float posY = transform.position.y;
+        _leftPoint.position = new Vector2((posX - (_renderer.size.x / 2) * transform.localScale.x),posY);
+        _rightPoint.position = new Vector2((posX + (_renderer.size.x / 2) * transform.localScale.x),posY);
+        _upPoint.position = new Vector2(posX,posY+ (_renderer.size.y / 2) * transform.localScale.y);
+        _downPoint.position = new Vector2(posX,posY - (_renderer.size.y / 2)*transform.localScale.y);
+
+        _leftWall = GameObject.Find("LeftWall").transform.position;
+        _rightWall = GameObject.Find("RightWall").transform.position;
+        _upWall = GameObject.Find("UpWall").transform.position;
+        _downWall= GameObject.Find("DownWall").transform.position;
+        SetLayer();
+    }
+
+    void SetLayer()
     {
         switch (_layerType)
         {
+            case LayerType.Default:
+                gameObject.layer = LayerMask.NameToLayer("Default");
+                gameObject.tag = "Untagged";
+                _renderer.sortingLayerName = "Default";
+                break;
             case LayerType.Background:
+                gameObject.layer = LayerMask.NameToLayer("Background");
+                gameObject.tag = "Background";
+                _renderer.sortingLayerName = "Background";
+                break;
+            case LayerType.Middleground:
+                gameObject.layer = LayerMask.NameToLayer("Middleground");
+                _renderer.sortingLayerName = "Middleground";
+                break;
+            case LayerType.Foreground:
+                gameObject.layer = LayerMask.NameToLayer("Foreground");
+                _renderer.sortingLayerName = "Foreground";
                 break;
         }
     }
-
-#if true
     public void MoveX(float delta)
     {
         Vector3 newPos = transform.localPosition;
         newPos.x -= delta * _horizontalFactor;
-
-        transform.localPosition = newPos;
+        transform.position = newPos;
     }
     
     public void MoveY(float delta)
     {
         Vector3 newPos = transform.localPosition;
         newPos.y -= delta * _verticalFactor;
-
-        transform.localPosition = newPos;
+        transform.position = newPos;
     }
-#endif
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void Update()
     {
-        Debug.Log(1);
+        if (transform.localScale.x != _spriteSize)
+        {
+            transform.localScale = new Vector2(_spriteSize, _spriteSize);
+        }
+        
+        SetLayer();
     }
 }
