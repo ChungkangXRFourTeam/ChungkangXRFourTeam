07687c221389162d2e64fdf60190e6e97cb750b0
SeoBeomSeok <warcry4523@gmail.com>
2023-11-03 16:28:24 +0900

    feat : 이미지 크기 자동 조정 스크립트 추가
    기존에 이미지를 유니티에서 불러오게 하면 최대 크기가 2048로 조정되었음.
    그러나 원경의 경우 최대 5160 크기를 가지기 때문에 일일이 조정하는 것은 불편함.
    따라서 스크립트를 통해 이미지를 유니티에서 가져오게 하면 자동으로 8192 사이즈로 변경되는 스크립트를 작성함.

---
+++ Assets/Develop/Script/Utils/Editor/XRCustomEditor/Texture2DSizeSetter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Texture2DSizeSetter : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        //get a reference to the built-in TextureImporter...
+        TextureImporter importer = (TextureImporter)assetImporter;
+
+        //create a new empty TextureImporterSettings struct...
+        TextureImporterSettings textureImporterSettings = new TextureImporterSettings();
+
+        //read the current import settings from the Texture Importer
+        //into our new importer settings struct (basically filling the empty struct with values)
+        importer.ReadTextureSettings(textureImporterSettings);
+
+        //change the maxTextureSize setting in our settings struct
+        textureImporterSettings.maxTextureSize = 8192;
+        textureImporterSettings.sRGBTexture = true;
+
+        //pass the settings struct, with the changed maxTextureSize value, back into the importer
+        //(e.g. apply the changed settings to the importer)
+        importer.SetTextureSettings(textureImporterSettings);
+    }
+}
