e6a4b95dc5e48bdef03d8589b1d89e174eabc378
seoBumseok <warcry4523@gmail.com>
2023-11-29 00:39:05 +0900

    feat : 몬스터 카운트 스크립트 추가
    클리어시 켄넬 소환을 위한 스크립트 및 프리팹 추가

---
+++ Assets/Develop/Script/Enemy/MonsterCountManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MonsterCountManager : MonoBehaviour
+{
+    private int _monsterCount;
+    [SerializeField]
+    private GameObject _kennelPrefab;
+
+    [SerializeField] private Transform _spawnPoint; 
+
+    [SerializeField] private GameObject _player;
+
+    private void Awake()
+    {
+        ChangeMonsterCount();
+    }
+
+    private void ChangeMonsterCount()
+    {
+        _monsterCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        if (_monsterCount == 0)
+            Instantiate(_kennelPrefab, _spawnPoint.position ,quaternion.identity);
+    }
+}
