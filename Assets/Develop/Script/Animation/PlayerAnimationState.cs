7e7e733de3747eaf235426008414ff26d5706d51
DESKTOP-9TC8CGS\hyeon2-309-13 <d>
2023-11-29 07:20:56 +0900

    카메라 수케ㅣㅇ

--- Assets/Develop/Script/Animation/PlayerAnimationState.cs
+++ Assets/Develop/Script/Animation/PlayerAnimationState.cs
@@ -133,6 +133,7 @@ public class PlayerAnimationState : MonoBehaviour
 
         _playerController.SetPropertiesCount(_playerController.Interaction.ContractInfo,_playerController.RemainingPropertie - 1);
         enemy.DoHit(_interaction.ContractInfo,damage);
+        VirtualCameraShaker.Instance.CameraShake(0.25f, 3f, 1f);
     }
 
     private bool _isAttacking;
